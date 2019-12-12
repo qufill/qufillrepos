06c758d6a5d744785564791ad18ebefaa6263f25
qufill.max <ishe.jpg@gmail.com>
2019-12-12 13:08:19 +0300

    Added sum of numbers

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,7 +6,11 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Господи оно работает");
+            int x = 5;
+            int y = 12;
+            Console.WriteLine("5 + 12 = {0}", x + y);
+            Console.ReadLine();
         }
     }
 }
